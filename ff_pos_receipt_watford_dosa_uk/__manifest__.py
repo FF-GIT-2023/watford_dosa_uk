e62eacf4e76bbcffc0596e14f54dca81eff6a700
Vyshnav9062 <vyshnav9062@gmail.com>
2025-07-21 10:51:22 +0530

    [ADD]:ff_pos_receipt_watford_dosa_uk

---
+++ ff_pos_receipt_watford_dosa_uk/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "POS Receipt Extend",
+    "version": "17.0.1.0.1",
+    "category": "Point of Sale",
+    "summary": "Customizing receipt footer",
+    "description": """Customizing receipt footer""",
+    "author": "ForeFront Technologies",
+    "company": "ForeFront Technologies",
+    "maintainer": "ForeFront Technologies",
+    "depends": ["point_of_sale"],
+    "data": [],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "ff_pos_receipt_watford_dosa_uk/static/src/xml/OrderReceipt.xml",
+        ]
+    },
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
